76ed875dddf17ab4a10b05082af512d295f9669f
paascl <eikenaartje@msn.com>
2013-12-26 14:09:06 +0100

    .

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>OBK Bennekom</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-?>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
