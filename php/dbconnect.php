8d321164efcd553debf8d99e5bfecf3f61f1c2cc
paascl <eikenaartje@msn.com>
2014-01-10 19:10:56 +0100

    10-01-2014

---
+++ php/dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbname = "obkbennekom";
+$username = "0bkappus3r";
+$password = "Kierkamp36";
+$hostname = "localhost"; 
+
+//connection to the database
+$conn = mysql_connect($hostname, $username, $password, $dbname) 
+  or die("Database niet beschikbaar.");
+?>
+
+
